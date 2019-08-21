246bbdc7917e3d84f8e146dd0702453063cfccfa
a.neposedov <a.neposedov@concert.ua>
2019-08-21 12:01:19 +0300

    registration fixed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,7 +45,7 @@ class RegisterController extends Controller
      */
     public function showRegistrationForm()
     {
-        return redirect('register');
+        return view('auth.register');
     }
 
     /**
